5ae109c0f5319ea2491f9aeeda3ea92e700e8fcf
Yasmin <yasmin.carolina12@gmail.com>
2022-06-20 10:18:19 -0300

    create game

--- begappmyadmin/lib/classes/database.dart
+++ begappmyadmin/lib/classes/database.dart
@@ -9,7 +9,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class Database {
   // static const defaultUrl = "https://localhost:44370/api/v1/";
-  var defaultUrl = "https://api.begapp.com.br/api/v1/";
+  static const defaultUrl = "https://api.begapp.com.br/api/v1/";
 
   static getToken() async {
     localStorage = await SharedPreferences.getInstance();
